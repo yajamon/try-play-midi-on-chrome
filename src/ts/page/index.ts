2f46cc28dadc5c5b2a6b6824b74ecc37cfd389db
yajamon <yajamon.tatsuki@gmail.com>
2016-11-03 23:37:19 +0900

    ocrillatorで音を再生する

--- src/ts/page/index.ts
+++ src/ts/page/index.ts
@@ -1,3 +1,8 @@
-console.log('hello');
+let audioCtx = new AudioContext();
+let oscillator =  audioCtx.createOscillator();
 
+oscillator.type = "sine";
+oscillator.frequency.value = 440;
+oscillator.connect(audioCtx.destination);
 
+oscillator.start();
