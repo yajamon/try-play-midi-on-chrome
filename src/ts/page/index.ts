88216240a63d10e499894695c14c78a0bbb1679f
yajamon <yajamon.tatsuki@gmail.com>
2016-11-11 01:17:49 +0900

    OscillatorNodeの管理をVue.jsでできるようにした

--- src/ts/page/index.ts
+++ src/ts/page/index.ts
@@ -1,15 +1,34 @@
+/// <reference path="../../../typings/vue/vue.d.ts" />
+
 let audioCtx = new AudioContext();
 
-window.addEventListener("load", () => {
-    var oscillator: OscillatorNode;
-    document.getElementsByClassName("play")[0].addEventListener("click", () => {
-        oscillator = audioCtx.createOscillator();
-        oscillator.type = "sine";
-        oscillator.frequency.value = 440;
-        oscillator.connect(audioCtx.destination);
-        oscillator.start();
+window.addEventListener('load', () => {
+    let vm = new Vue({
+        el: "#app",
+        data: {
+            audioNodes: [],
+        },
+        methods: {
+            addOscillator: function () {
+                let oscillator = audioCtx.createOscillator();
+                oscillator.type = "sine";
+                oscillator.frequency.value = 440;
+                oscillator.connect(audioCtx.destination);
+                oscillator.start();
+                this.audioNodes.push(oscillator);
+            },
+            removeNode: function (node:AudioNode) {
+                let nodes: AudioNode[] = this.$data.audioNodes;
+
+                if (node instanceof OscillatorNode) {
+                    node as OscillatorNode;
+                    node.stop();
+                }
+                node.disconnect();
+                nodes.$remove(node);
+            },
+        },
     });
-    document.getElementsByClassName("pause")[0].addEventListener("click", () => {
-        oscillator.stop();
-    });
+    
 });
+
