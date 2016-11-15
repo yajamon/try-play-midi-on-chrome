4229bcda3ba7963a45811590aaf81c21b8ed168d
yajamon <yajamon.tatsuki@gmail.com>
2016-11-16 08:32:48 +0900

    wip

--- src/ts/page/index.ts
+++ src/ts/page/index.ts
@@ -1,12 +1,61 @@
 /// <reference path="../../../typings/vue/vue.d.ts" />
 
 let audioCtx = new AudioContext();
+let audioNodes: AudioNode[] = [];
+let analyser = audioCtx.createAnalyser();
+let canvas: HTMLCanvasElement;
+let canvasCtx: CanvasRenderingContext2D;
+
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+
+function drawWave() {
+    let drawVisual = requestAnimationFrame(drawWave);
+    let WIDTH = canvas.width;
+    let HEIGHT = canvas.height;
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+
+    for (var i = 0; i < bufferLength; i++) {
+
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT / 2;
+
+        if (i === 0) {
+            canvasCtx.moveTo(x, y);
+        } else {
+            canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+}
 
 window.addEventListener('load', () => {
+    canvas = document.getElementById("waveRender") as HTMLCanvasElement;
+    canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    drawWave();
+
     let vm = new Vue({
         el: "#app",
         data: {
-            audioNodes: [],
+            audioNodes: audioNodes,
             oscillatorTypes: [
                 "sine",
                 "square",
@@ -20,6 +69,8 @@ window.addEventListener('load', () => {
                 oscillator.type = "sine";
                 oscillator.frequency.value = 440;
                 oscillator.start();
+
+                oscillator.connect(analyser);
 
                 let gain = audioCtx.createGain();
                 oscillator.connect(gain);
