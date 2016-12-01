13cb55aa4f63357d469e19576fc58d5fe9182764
yajamon <yajamon.tatsuki@gmail.com>
2016-12-01 17:25:05 +0900

    Edit gulpfile
    cleaning before build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 /// <reference path="node_modules/@types/gulp-typescript/index.d.ts" />
 let gulp = require('gulp');
 let ts = require('gulp-typescript');
+let del = require('del');
 
 let paths = {
     src: {
@@ -17,7 +18,6 @@ let paths = {
 };
 
 let tsProject = ts.createProject('src/ts/tsconfig.json', {
-    out: 'app.js',
 });
 
 gulp.task('default', ['build']);
@@ -29,17 +29,20 @@ gulp.task('watch', ['watch:ts', 'watch:html']);
 // sub tasks
 
 gulp.task('build:ts', function() {
+    del([paths.dist.js]);
     let tsResult = gulp.src(paths.src.ts)
         .pipe(tsProject());
     tsResult.js.pipe(gulp.dest(paths.dist.js));
 });
 
 gulp.task('build:html', function() {
+    del([paths.dist.html]);
     gulp.src(paths.src.html)
         .pipe(gulp.dest(paths.dist.html));
 });
 
 gulp.task('build:vendor', function() {
+    del([paths.dist.vendor]);
     gulp.src(paths.src.vendor)
         .pipe(gulp.dest(paths.dist.vendor));
 });
