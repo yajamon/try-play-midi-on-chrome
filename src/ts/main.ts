e34cd4226c2ee71fa6e6f6f8485bb23f364bd82d
yajamon <yajamon.tatsuki@gmail.com>
2016-12-01 20:00:36 +0900

    Add using requirejs main logic

---
+++ src/ts/main.ts
@@ -0,0 +1,13 @@
+
+var requireConfig:RequireConfig = {
+    baseUrl: "../js",
+    paths: {
+        'react': '../vendor/react',
+        'react-dom': '../vendor/react-dom',
+        'domReady': '../vendor/ready',
+    }
+};
+require.config(requireConfig);
+
+require(['index'],function(){
+});
