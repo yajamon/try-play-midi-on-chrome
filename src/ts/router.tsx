2040308028bcdf37d4a6cab3a609c44e15106226
yajamon <yajamon.tatsuki@gmail.com>
2017-06-13 20:52:28 +0900

    Instead component from Router to Application

--- src/ts/router.tsx
+++ src/ts/router.tsx
@@ -3,15 +3,8 @@ import { render } from 'react-dom';
 import {Route, IndexRoute, Router, hashHistory} from 'react-router';
 
 import { Application } from './Application';
-import { Index } from './components/Index';
-import { Hello } from "./components/Hello";
 
 render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Application} >
-            <IndexRoute component={Index} ></IndexRoute>
-            <Route path="/hello" component={Hello} ></Route>
-        </Route>
-    </Router>,
+    <Application />,
     document.getElementById("app")
 );
